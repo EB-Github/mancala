8bd8201465c433d6685541d28e3231df6ea0f549
EB-Github <ebrown6@uab.edu>
2017-04-04 23:51:17 -0500

    added draw

--- mancala.cpp
+++ mancala.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 class Board {
 	int row_player1[6] = {4,4,4,4,4,4};
 	int row_player2[6] = {4,4,4,4,4,4};
@@ -7,7 +8,8 @@ class Board {
 	int player2_store;
 	public:
 		void init();
-		void draw(); 
+		void draw();
+		void move(int, int); 
 };
 void Board::init(){
 	player1_store = 0;
@@ -15,8 +17,21 @@ void Board::init(){
 }
 
 void Board::draw(){
-
+	std::cout << "Player 1: (" << player1_store << ")";
+	for(int i = 0; i < 6; i++){
+		std::cout << "[" << row_player1[i] << "]";
+	};
+	std::cout << "\n";
+	std::cout << "Player 2:    ";
+	for(int i = 0; i < 6; i++){
+                std::cout << "[" << row_player2[i] << "]";
+        };
+	std::cout << "(" << player2_store << ")";
+	std::cout << "\n";
 }
+
 int main(int argc, char* argv[]){
-	
+	Board b;
+	b.init();
+	b.draw();
 }
